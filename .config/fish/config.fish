a404c6f6560f9e7c28f4c131ba878b2ff84a6130
Brian Hsu <brianhsu@berkeley.edu>
2019-08-24 17:41:58 -0700

    Initial dump

---
+++ .config/fish/config.fish
@@ -0,0 +1,34 @@
+alias csr="cd ~/CS/Research"
+alias cs="cd ~/CS"
+alias vir="ssh panda@192.3.59.138 -p 24011"
+alias d100="cd ~/CS/data100"
+alias 162="cd ~/CS/CS162"
+alias pacman="sudo pacman"
+alias openvpn="sudo openvpn"
+alias docker186="docker run -v ~/CS/CS186:/cs186 -it cs186/environment bash"
+alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+source /home/brian/miniconda2/etc/fish/conf.d/conda.fish
+
+set PATH /home/brian/miniconda2/bin :$PATH
+
+set ANDROID_HOME $HOME/Android/Sdk
+set PATH $ANDROID_HOME/emulator :$PATH
+set PATH $ANDROID_HOME/tools :$PATH
+set PATH $ANDROID_HOME/tools/bin :$PATH
+set PATH $ANDROID_HOME/platform-tools :$PATH
+
+function gp
+        git push
+end
+
+function update
+        git pull --rebase
+end
+
+function gc
+        git commit -a -m "$argv"
+end
+
+function ga
+        git add 
+end
