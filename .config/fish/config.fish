5f74070321cfbc4d724f4e8ab75dfa17981dfca5
Brian Hsu <brianhsu@berkeley.edu>
2019-12-02 16:49:47 -0800

    Not sure what i updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,22 +7,22 @@ alias openvpn="sudo openvpn"
 alias docker186="docker run -v ~/CS/CS186:/cs186 -it cs186/environment bash"
 alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias xclip='xclip -selection c'
-#source /home/brian/anaconda3/etc/fish/conf.d/conda.fish
-#set PATH /home/brian/anaconda3/bin :$PATH
+source /home/brian/anaconda3/etc/fish/conf.d/conda.fish
+set PATH /home/brian/anaconda3/bin :$PATH
 
-set ANDROID_HOME $HOME/Android/Sdk
-set PATH $ANDROID_HOME/emulator :$PATH
-set PATH $ANDROID_HOME/tools :$PATH
-set PATH $ANDROID_HOME/tools/bin :$PATH
-set PATH $ANDROID_HOME/platform-tools :$PATH
+#set ANDROID_HOME $HOME/Android/Sdk
+#set PATH $ANDROID_HOME/emulator :$PATH
+#set PATH $ANDROID_HOME/tools :$PATH
+#set PATH $ANDROID_HOME/tools/bin :$PATH
+#set PATH $ANDROID_HOME/platform-tools :$PATH
 
-set PATH /home/linuxbrew/.linuxbrew/bin :$PATH
-set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
-set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
-set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
-set -g fish_user_paths "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" $fish_user_paths;
-set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH;
-set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH;
+#set PATH /home/linuxbrew/.linuxbrew/bin :$PATH
+#set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
+#set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
+#set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
+#set -g fish_user_paths "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" $fish_user_paths;
+#set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH;
+#set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH;
 
 
 function gp
--- README.md
+++
@@ -1,6 +0,0 @@
-# dotfiles
-My dotfiles
-
-Right now, I'm using Manjaro Linux. My desktop setup is i3 + polybar + rofi.
-
-Tracked using [this guide](https://www.anand-iyer.com/blog/2018/a-simpler-way-to-manage-your-dotfiles.html)
